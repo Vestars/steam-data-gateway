6747141d44e1dd922e490b6b7a9a0baf03f1cd2a
Dmytro Voronin <vestars093@gmail.com>
2025-04-30 18:00:47 +0300

    Initial commit

---
+++ src/main/java/com/vestars/steam_data_gateaway/SteamDataGateawayApplication.java
@@ -0,0 +1,13 @@
+package com.vestars.steam_data_gateaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamDataGateawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamDataGateawayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vestars/steam_data_gateaway/SteamDataGateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vestars.steam_data_gateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamDataGateawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
